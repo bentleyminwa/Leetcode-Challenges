27bb01cab6855fefd4133ba433078c0b4d5f6887
bentleyminwa <bentleyminwa@gmail.com>
2024-12-18 16:56:11 +0300

    added utility functions

---
+++ main.js
@@ -0,0 +1,61 @@
+// function that takes in any number of arguments and returns the sum of all those numbers
+function add(...args) {
+  let sum = 0;
+
+  // for (let i = 0; i < args.length; i++) {
+  //   sum += args[i];
+  // }
+
+  for (num of args) {
+    sum += num;
+  }
+
+  return sum;
+}
+
+// console.log("sum all", add(2, 3, 4, 5));
+
+function getNames(users) {
+  const names = users.map((user) => {
+    // evaluates to true if the object contains the property 'name'
+    if (user.hasOwnProperty("name")) {
+      return user.name;
+    } else {
+      console.log("user has no property 'name'.");
+    }
+  });
+  return names;
+}
+
+const userNames = getNames([
+  {
+    id: 1,
+    name: "Daryl",
+  },
+  {
+    id: 2,
+    name: "Rick",
+  },
+  {
+    id: 3,
+    name: "Maggie",
+  },
+]);
+
+console.log(userNames);
+
+const [pokemonList, setPokemonList] = useState([]);
+
+async function getPokemonList() {
+  const response = await fetch(url);
+  return response.json();
+}
+
+useEffect(() => {
+  async function fetchData() {
+    const data = await getPokemonList();
+    setPokemonList(data);
+  }
+
+  fetchData();
+}, []);
