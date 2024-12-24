10741654eece291261e54d8ae7977df5fe77588c
bentleyminwa <bentleyminwa@gmail.com>
2024-12-24 17:38:23 +0300

    Apply transform over each element of an array

--- main.js
+++ main.js
@@ -25,3 +25,33 @@ function expect(val) {
 
 const result = expect(1).toBe(1); // true
 const result2 = expect(1).notToBe(2); // true
+
+// ============================== Question 2 ============================== //
+
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+// Please solve it without the built-in Array.map method.
+
+// ============================== Solution ============================== //
+
+// arr - an array of elements/integers we want to transform
+// fn - a mapping function that will be applied to each element
+// arr[i] - current element in the array
+// i - index of the current element
+
+function mapArray(arr, fn) {
+  let output = [];
+  for (let i = 0; i < arr.length; i++) {
+    output.push(fn(arr[i], i));
+  }
+  return output;
+}
+
+const numbers = [1, 2, 3, 4];
+
+function multiplyBy2(num) {
+  return num * 2;
+}
+
+const doubled = mapArray(numbers, multiplyBy2); // [2, 4, 6, 8]
+console.log(doubled);
