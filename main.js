acb95a671658fe78270c2dd426cda94f1fc91c3c
bentleyminwa <bentleyminwa@gmail.com>
2024-12-24 17:57:41 +0300

    added comments on first leet problem

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// ============================== Question 1 ============================== //
+
 // Write a function expect that helps developers test their code.
 // It should take in any value val and return an object with the following two functions.
 
@@ -5,6 +7,8 @@
 // If they are not equal, it should throw an error "Not Equal".
 // notToBe(val) accepts another value and returns true if the two values !== each other.
 // If they are equal, it should throw an error "Equal"
+
+// ============================== Solution ============================== //
 
 function expect(val) {
   return {
